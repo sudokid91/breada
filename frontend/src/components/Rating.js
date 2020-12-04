4b3177e312aab47dfd2d97c673ab978df858bfcf
hoangnn <hoangnn@bap.jp>
2020-12-04 17:14:11 +0700

    hoangnn commit product detail screen

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,6 +1,5 @@
 const Rating = (props) => {
     let {rating, numReviews} = props;
-    console.log(`rating: ${JSON.stringify(rating)}`);
     return (
         <div className="rating">
             <span> <i className={ rating >=1 ? "fa fa-star" : rating>=0.5 ? "fa fa-star-half-o" : "fa fa-star-o"}></i> </span>
